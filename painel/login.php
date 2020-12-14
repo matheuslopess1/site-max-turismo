0aa5767025fc98abbd13d673f17aac0f478350da
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-13 22:50:08 -0300

    altera título de login

--- painel/login.php
+++ painel/login.php
@@ -27,7 +27,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>ML App - Login</title>
     </head>
     <body>
         <h1>ML App</h2>
