c3234ea7a631f6af33560205cec549b4699ff37c
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-15 15:41:17 -0300

    adiciona nome do usuário em nav

--- painel/components/nav.php
+++ painel/components/nav.php
@@ -8,10 +8,13 @@
         </a>
     </div>
     <div class="navbar-menu">
-        <div class="navbar-end">
+        <div class="navbar-start">
             <a class="navbar-item" href="/painel/">Página Inicial</a>
             <a class="navbar-item" href="/painel/usuarios.php">Usuários</a>
             <a class="navbar-item" href="/painel/bancos.php">Bancos</a>
+        </div>
+        <div class="navbar-end">
+            <span class="navbar-item"><?=$_SESSION["usuario"]["nome"] ?></a>
             <a class="navbar-item has-text-danger" href="/painel/logout.php">Sair</a>
         </div>
     </div>
