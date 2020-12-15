87170da65f54bf24f26010e2057ab5c351c3cc19
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-15 15:28:16 -0300

    corrige colorização do fundo

--- painel/index.php
+++ painel/index.php
@@ -10,27 +10,29 @@
         <title>ML App - Dashboard</title>
         <?php include_once("utils/bulma_link.php"); ?>
     </head>
-    <body class="has-background-white-ter">
-        <?php include_once("components/nav.php"); ?>
-        <div class="container">
-            <h1>Dashboard</h1>
-            <div class="box">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <td>Nome</td>
-                            <td>Email</td>
-                            <td>Tipo</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><?=$_SESSION["usuario"]["nome"] ?></td>
-                            <td><?=$_SESSION["usuario"]["email"] ?></td>
-                            <td><?=$_SESSION["usuario"]["tipo"] ?></td>
-                        </tr>
-                    </tbody>
-                </table>
+    <body>
+        <div class="has-background-white-ter" style="height: 100vw;">
+            <?php include_once("components/nav.php"); ?>
+            <div class="container">
+                <h1>Dashboard</h1>
+                <div class="box">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <td>Nome</td>
+                                <td>Email</td>
+                                <td>Tipo</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?=$_SESSION["usuario"]["nome"] ?></td>
+                                <td><?=$_SESSION["usuario"]["email"] ?></td>
+                                <td><?=$_SESSION["usuario"]["tipo"] ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </body>
