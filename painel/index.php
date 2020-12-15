4121c77455d12c41cced72576e15b8076052aa65
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-15 15:22:46 -0300

    estiliza index

--- painel/index.php
+++ painel/index.php
@@ -8,25 +8,28 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>ML App - Dashboard</title>
+        <?php include_once("utils/bulma_link.php"); ?>
     </head>
-    <body>
-        <h1>ML App</h2>
-        <h2>Dashboard</h2>
+    <body class="has-background-white-ter">
         <?php include_once("components/nav.php"); ?>
-        <table>
-            <thead>
-                <tr>
-                    <td>Nome</td>
-                    <td>Email</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><?=$_SESSION["usuario"]["nome"] ?></td>
-                    <td><?=$_SESSION["usuario"]["email"] ?></td>
-                    <td><?=$_SESSION["usuario"]["tipo"] ?></td>
-                </tr>
-            </tbody>
-        </table>
+        <h1>Dashboard</h1>
+        <div class="box">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <td>Nome</td>
+                        <td>Email</td>
+                        <td>Tipo</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?=$_SESSION["usuario"]["nome"] ?></td>
+                        <td><?=$_SESSION["usuario"]["email"] ?></td>
+                        <td><?=$_SESSION["usuario"]["tipo"] ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
