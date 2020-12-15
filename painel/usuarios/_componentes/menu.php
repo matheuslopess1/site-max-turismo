3b3a022cc82164165413e4fab7540ba71adbf4ac
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-15 20:54:09 -0300

    exclui painel

--- painel/usuarios/_componentes/menu.php
+++
@@ -1,7 +0,0 @@
-<div class="menu has-background-white">
-    <p class="menu-label">Usuários</p>
-    <ul class="menu-list">
-        <li><a href="/painel/usuarios/">Listar</a></li>
-        <li><a href="/painel/usuarios/criar/">Criar</a></li>
-    </ul>
-</div>
