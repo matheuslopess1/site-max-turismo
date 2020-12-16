fbc539c44e2a22e06e370560e6014f252b6fb062
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-15 21:54:16 -0300

    cria dashboard de painel

---
+++ painel/index.php
@@ -0,0 +1,26 @@
+<?php
+    if (!isset($_SESSION)) session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dashboard</title>
+    </head>
+    <body>
+        <h1>ML App</h1>
+        <h2>Dashboard</h2>
+        <ul>
+            <li><a href="/painel/index.php">Dashboard</a></li>
+            <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
+                <li><a href="/painel/bancos_listagem.php">Bancos</a></li>
+                <li><a href="/painel/bancos_listagem.php">Usuários</a></li>
+            <?php } ?>
+        </ul>
+        <ul>
+            <li><?= $_SESSION["usuario"]["nome"] ?></li>
+            <li><a href="/painel/logout.php">Sair</a></li>
+        </ul>
+    </body>
+</html>
