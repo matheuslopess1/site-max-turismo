20837d21fdf70238d74a614a9e2b9b1b0dbbf99b
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-15 22:39:00 -0300

    corrige link para usuarios

--- painel/bancos_listagem.php
+++ painel/bancos_listagem.php
@@ -25,7 +25,7 @@
             <li><a href="/painel/index.php">Dashboard</a></li>
             <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
                 <li><a href="/painel/bancos_listagem.php">Bancos</a></li>
-                <li><a href="/painel/bancos_listagem.php">Usuários</a></li>
+                <li><a href="/painel/usuarios_listagem.php">Usuários</a></li>
             <?php } ?>
         </ul>
         <h2>Bancos</h2>
--- painel/index.php
+++ painel/index.php
@@ -20,7 +20,7 @@
             <li><a href="/painel/index.php">Dashboard</a></li>
             <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
                 <li><a href="/painel/bancos_listagem.php">Bancos</a></li>
-                <li><a href="/painel/bancos_listagem.php">Usuários</a></li>
+                <li><a href="/painel/usuarios_listagem.php">Usuários</a></li>
             <?php } ?>
         </ul>
         <h2>Dashboard</h2>
