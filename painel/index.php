542c94a79154be7ca70975ccd989dbf4b6c4f22f
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-15 21:55:19 -0300

    impede usuario nao atenticado entrar no dashboard

--- painel/index.php
+++ painel/index.php
@@ -1,5 +1,11 @@
 <?php
-    if (!isset($_SESSION)) session_start();
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    if (!isset($_SESSION["autenticado"])) {
+        header("Location: login.php");
+        exit();
+    }
 ?>
 <!DOCTYPE html>
 <html>
