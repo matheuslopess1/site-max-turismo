cd62140cd2fa0dfa09b60625fc6916a770ee30a9
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-17 07:55:21 -0300

    adiciona agencias no nav

--- painel/usuarios_listagem.php
+++ painel/usuarios_listagem.php
@@ -28,8 +28,9 @@
         <ul>
             <li><a href="/painel/index.php">Dashboard</a></li>
             <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
-                <li><a href="/painel/bancos_listagem.php">Bancos</a></li>
                 <li><a href="/painel/usuarios_listagem.php">Usuários</a></li>
+                <li><a href="/painel/bancos_listagem.php">Bancos</a></li>
+                <li><a href="/painel/agencias_listagem.php">Agências</a></li>
             <?php } ?>
         </ul>
         <ul>
