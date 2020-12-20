46098bb00fe353e650c53429f987756765f33df9
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-20 20:00:23 -0300

    altera estilo em index

--- painel/index.php
+++ painel/index.php
@@ -10,7 +10,7 @@
         <title>Dashboard</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     </head>
-    <body>
+    <body class="bg-secondary">
         <div class="container">
             <h1>ML App</h1>
             <?php include_once("componentes/nav.php"); ?>
