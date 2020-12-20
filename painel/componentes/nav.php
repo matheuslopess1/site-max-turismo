0bf845108e0f140b00029cedcc1805829d8802bb
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-20 20:30:18 -0300

    altera estilo em nav

--- painel/componentes/nav.php
+++ painel/componentes/nav.php
@@ -1,26 +1,11 @@
-<div class="d-flex justify-content-between mb-3">
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link" href="/painel/index.php">Dashboard</a>
-        </li>
-        <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
-            <li class="nav-item">
-                <a class="nav-link" href="/painel/usuarios_listagem.php">Usuários</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/painel/bancos_listagem.php">Bancos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/painel/agencias_listagem.php">Agências</a>
-            </li>
-        <?php } ?>
-    </ul>
-    <ul class="nav">
-        <li class="nav-item">
-            <span class="nav-link"><?= $_SESSION["usuario"]["nome"] ?></span>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-secondary" href="/painel/logout.php">sair</a>
-        </li>
-    </ul>
-</div>
+<nav class="nav">
+    <a class="nav-link" href="/painel/index.php">Dashboard</a>
+    <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
+        <a class="nav-link" href="/painel/usuarios_listagem.php">Usuários</a>
+        <a class="nav-link" href="/painel/bancos_listagem.php">Bancos</a>
+        <a class="nav-link" href="/painel/agencias_listagem.php">Agências</a>
+    <?php } ?>
+    <div class="ml-auto"></div>
+    <span class="nav-link"><?= $_SESSION["usuario"]["nome"] ?></span>
+    <a class="nav-link text-secondary" href="/painel/logout.php">sair</a>
+</nav>
