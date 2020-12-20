b4288e920dd6dbd5fa6d23912ce1ccec68fc80d9
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-20 20:58:41 -0300

    altera vh-100 para min-vh-100

--- painel/index.php
+++ painel/index.php
@@ -11,7 +11,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     </head>
     <body class="bg-secondary">
-        <div class="container bg-white vh-100">
+        <div class="container bg-white min-vh-100">
             <h1 class="text-center py-5">ML App</h1>
             <?php include_once("componentes/nav.php"); ?>
             <h2>Dashboard</h2>
--- painel/usuarios_listagem.php
+++ painel/usuarios_listagem.php
@@ -17,7 +17,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     </head>
     <body class="bg-secondary">
-        <div class="container bg-white vh-100">
+        <div class="container bg-white min-vh-100">
             <h1 class="text-center py-5">ML App</h1>
             <?php include_once("componentes/nav.php"); ?>
             <div class="d-flex justify-content-between align-items-center mb-3">
