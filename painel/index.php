702c91cde14b8c8ad82aad13ce01df09b674be29
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-20 19:58:41 -0300

    adiciona bootstrap em index

--- painel/index.php
+++ painel/index.php
@@ -8,10 +8,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     </head>
     <body>
-        <h1>ML App</h1>
-        <?php include_once("componentes/nav.php"); ?>
-        <h2>Dashboard</h2>
+        <div class="container">
+            <h1>ML App</h1>
+            <?php include_once("componentes/nav.php"); ?>
+            <h2>Dashboard</h2>
+        </div>
     </body>
 </html>
