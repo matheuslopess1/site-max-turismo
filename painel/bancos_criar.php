5d52a05e038141578f5f986112bb52627503a808
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-20 23:06:46 -0300

    altera estilos em bancos criar com bootstrap

--- painel/bancos_criar.php
+++ painel/bancos_criar.php
@@ -30,29 +30,38 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>ML App - Criar Banco</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     </head>
-    <body>
-        <h1>ML App</h1>
-        <?php include_once("componentes/nav.php"); ?>
-        <h2>Bancos</h2>
-        <ul>
-            <li><a href="/painel/bancos_listagem.php">Listagem</a></li>
-            <li><a href="/painel/bancos_criar.php">Criar</a></li>
-        </ul>
-        <h3>Criação</h3>
-        <form method="POST">
-            <?php if (isset($erro)) { ?>
-                <p style="color: red;"><?= $erro ?></p>
-            <?php } ?>
-            <p>
-                <label for="codigo">Código*</label>
-                <input type="number" id="codigo" name="codigo" min="1" max="999" required />
-            </p>
-            <p>
-                <label for="nome">Nome*</label>
-                <input type="text" id="nome" name="nome" maxlength="50" required autofocus />
-            </p>
-            <button type="submit">Criar</button>
-        </form>
+    <body class="bg-secondary">
+        <div class="container bg-white min-vh-100">
+            <h1 class="text-center py-5">ML App</h1>
+            <?php include_once("componentes/nav.php"); ?>
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <h2 class="mb-0">Bancos</h2>
+                <div class="nav">
+                    <a class="nav-link" href="/painel/bancos_listagem.php">Listagem</a>
+                    <a class="nav-link" href="/painel/bancos_criar.php">Criar</a>
+                </div>
+            </div>
+            <h3>Criação</h3>
+            <form class="row g-3" method="POST">
+                <?php if (isset($erro)) { ?>
+                    <div class="text-danger"><?= $erro ?></div>
+                <?php } ?>
+                <div class="col-md-6">
+                    <label class="form-label" for="codigo">Código*</label>
+                    <input class="form-control" type="number" id="codigo" name="codigo" min="1" max="999" required />
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label" for="nome">Nome*</label>
+                    <input class="form-control" type="text" id="nome" name="nome" maxlength="50" required autofocus />
+                </div>
+                <div>
+                    <div class="d-grid d-md-block">
+                        <button class="btn btn-primary" type="submit">Criar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </body>
 </html>
