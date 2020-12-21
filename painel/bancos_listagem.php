c57584c775c36de90a456d689a60967279fc57f1
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-21 00:08:45 -0300

    altera bancos listagem

--- painel/bancos_listagem.php
+++ painel/bancos_listagem.php
@@ -3,6 +3,27 @@
     include_once("componentes/apenas_autenticado.php");
     include_once("componentes/apenas_admin.php");
     include_once("componentes/db.php");
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $nome = $_POST["nome"];
+        $codigo = $_POST["codigo"];
+        include_once("componentes/db.php");
+        $stmt = $mysqli->prepare("SELECT COUNT(*) AS contagem FROM bancos WHERE codigo = ?");
+        $stmt->bind_param("i", $codigo);
+        $stmt->execute();
+        $stmt->bind_result($contagem);
+        $stmt->fetch();
+        if ($contagem === 0) {
+            $stmt->close();
+            $stmt = $mysqli->prepare("INSERT INTO bancos (nome, codigo) VALUES (?, ?)");
+            $stmt->bind_param("si", $nome, $codigo);
+            $stmt->execute();
+        } else {
+            $erro = "Código já registrado";
+            $iniciar_modal_aberto = true;
+        }
+        $stmt->close();
+        $mysqli->close();
+    }
     $sql = "SELECT b.id, b.nome, b.codigo, (SELECT COUNT(*) FROM agencias WHERE banco_id = b.id) agencias FROM bancos b";
     $resultado = $mysqli->query($sql);
     $bancos = $resultado->fetch_all(MYSQLI_ASSOC);
@@ -22,13 +43,14 @@
             <div class="d-flex justify-content-between align-items-center mb-3">
                 <h2 class="mb-0">Bancos</h2>
                 <div class="nav">
-                    <a class="nav-link" href="/painel/bancos_listagem.php">Listagem</a>
-                    <a class="nav-link" href="/painel/bancos_criar.php">Criar</a>
+                    <button class="nav-link btn btn-success" onclick="toggleModalCriacao()">
+                        Criar
+                    </button>
                 </div>
             </div>
             <h3>Listagem</h3>
             <div class="table-responsive">
-                <table class="table">
+                <table class="table table-striped">
                     <thead>
                         <tr>
                             <th>#</th>
@@ -46,7 +68,7 @@
                                 <td><?= $banco["nome"] ?></td>
                                 <td><?= $banco["agencias"] ?></td>
                                 <td>
-                                    <a href="/painel/bancos_detalhe.php?id=<?= $banco["id"] ?>">
+                                    <a class="btn btn-info" href="/painel/bancos_detalhe.php?id=<?= $banco["id"] ?>">
                                         Ver mais
                                     </a>
                                 </td>
@@ -56,5 +78,44 @@
                 </table>
             </div>
         </div>
+        <div class="modal fade" id="modal-criacao" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 class="h5 modal-title">Criação</h3>
+                        <button class="btn-close" onclick="toggleModalCriacao()"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form class="row g-3" method="POST">
+                            <?php if (isset($erro)) { ?>
+                                <div class="text-danger"><?= $erro ?></div>
+                            <?php } ?>
+                            <div class="col-md-2">
+                                <label class="form-label" for="codigo">Código*</label>
+                                <input class="form-control" type="number" id="codigo" name="codigo" min="1" max="999" required />
+                            </div>
+                            <div class="col-md-8">
+                                <label class="form-label" for="nome">Nome*</label>
+                                <input class="form-control" type="text" id="nome" name="nome" maxlength="50" required autofocus />
+                            </div>
+                            <input type="submit" id="submit-form-criacao" class="d-none" />
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" onclick="toggleModalCriacao()">Close</button>
+                        <label class="btn btn-primary" for="submit-form-criacao" tabindex="0"></label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            function toggleModalCriacao () {
+                document.getElementById("modal-criacao").classList.toggle("show");
+            }
+
+            <?php if (isset($iniciar_modal_aberto)) { ?>
+                toggleModalCriacao();
+            <?php } ?>
+        </script>
     </body>
 </html>
