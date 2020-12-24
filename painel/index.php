f2e7df9fb5b308b9642f88546a7eab0077b6e7cd
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-23 23:44:05 -0300

    reformula index

--- painel/index.php
+++ painel/index.php
@@ -10,11 +10,13 @@
         <title>ML App - Dashboard</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     </head>
-    <body class="bg-secondary">
-        <div class="container bg-white min-vh-100">
-            <h1 class="text-center py-5">ML App</h1>
-            <?php include_once("componentes/nav.php"); ?>
+    <body class="bg-light">
+        <?php include_once("componentes/nav.php"); ?>
+        <div class="container ">
             <h2>Dashboard</h2>
+            <div class="bg-white py-3 px-2">
+                Bem vindo. Utilize o menu acima para navegar.
+            </div>
         </div>
     </body>
 </html>
