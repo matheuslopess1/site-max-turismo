dca4ef7d03ab14d7f567139b34b4e1bbb113fc56
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-23 23:43:57 -0300

    reformula nav

--- painel/componentes/nav.php
+++ painel/componentes/nav.php
@@ -1,8 +1,25 @@
-<div class="nav mb-3">
-    <a class="nav-link" href="/painel/index.php">Dashboard</a>
-    <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
-        <a class="nav-link" href="/painel/usuarios_listagem.php">Usuários</a>
-    <?php } ?>
-    <span class="nav-link ms-md-auto"><?= $_SESSION["usuario"]["nome"] ?></span>
-    <a class="nav-link text-secondary" href="/painel/logout.php">sair</a>
+<div class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">ML App</a>
+        <button
+            class="navbar-toggler"
+            onclick="document.querySelector('#navbar').classList.toggle('show')"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+            <div class="navbar-nav me-auto">
+                <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
+                    <a class="nav-item nav-link" href="/painel/usuarios_listagem.php">Usuários</a>
+                <?php } ?>
+            </div>
+            <hr class="bg-white my-2 d-lg-none" />
+            <span class="navbar-text">
+                <?= $_SESSION["usuario"]["nome"] ?>
+            </span>
+            <div class="navbar-nav">
+                <a class="nav-item nav-link text-secondary" href="/painel/logout.php">sair</a>
+            </div>
+        </div>
+    </div>
 </div>
