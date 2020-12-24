3dfb647da0bdbc0a4a72c2820039fac9a67c9ccf
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-23 22:32:07 -0300

    remove arquivos e atualiza o nav

--- painel/componentes/nav.php
+++ painel/componentes/nav.php
@@ -2,8 +2,6 @@
     <a class="nav-link" href="/painel/index.php">Dashboard</a>
     <?php if ($_SESSION["usuario"]["tipo"] === "ADMIN") { ?>
         <a class="nav-link" href="/painel/usuarios_listagem.php">Usuários</a>
-        <a class="nav-link" href="/painel/bancos_listagem.php">Bancos</a>
-        <a class="nav-link" href="/painel/agencias_listagem.php">Agências</a>
     <?php } ?>
     <span class="nav-link ms-md-auto"><?= $_SESSION["usuario"]["nome"] ?></span>
     <a class="nav-link text-secondary" href="/painel/logout.php">sair</a>
