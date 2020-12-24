005145f5d01cef7a1726bece0d648d90ed158f18
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-12-23 22:30:33 -0300

    reformula o login

--- painel/login.php
+++ painel/login.php
@@ -26,24 +26,42 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Dashboard</title>
+        <title>ML App - Login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     </head>
-    <body>
-        <h1>ML App</h1>
-        <h2>Login</h2>
-        <form method="POST">
-            <?php if (isset($erro)) { ?>
-                <p style="color: red;"><?= $erro ?></p>
-            <?php } ?>
-            <p>
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" required />
-            </p>
-            <p>
-                <label for="senha">Senha</label>
-                <input type="password" id="senha" name="senha" required />
-            </p>
-            <button type="submit">Entrar</button>
-        </form>
+    <body class="bg-secondary bg-gradient">
+        <div class="container min-vh-100">
+            <div class="row min-vh-100 align-items-center">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="card py-5">
+                        <div class="card-body">
+                            <div class="row bg-white">
+                                <div class="col-lg-6 d-flex justify-content-center align-items-center">
+                                    <h1>ML App</h1>
+                                </div>
+                                <div class="col-lg-6">
+                                    <form method="POST">
+                                        <?php if (isset($erro)) { ?>
+                                            <div class="text-danger"><?= $erro ?></div>
+                                        <?php } ?>
+                                        <div class="mb-3">
+                                            <input class="form-control" type="email" id="email" name="email" placeholder="Email" required />
+                                        </div>
+                                        <div class="mb-3">
+                                            <input class="form-control" type="password" id="senha" name="senha" placeholder="Senha" required />
+                                        </div>
+                                        <div class="d-grid">
+                                            <button class="btn btn-primary" type="submit">Entrar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
     </body>
 </html>
