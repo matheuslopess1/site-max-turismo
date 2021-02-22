5ded7035b128b3da7bfc017d11d2be79358d95e2
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-02-22 01:53:06 -0300

    atualiza dashboard

--- panel/dashboard/index.php
+++ panel/dashboard/index.php
@@ -1,7 +1,21 @@
 <?php
     include_once("./../components/session.php");
     include_once("./../components/authenticated.php");
+    include_once("./../components/database.php");
+
+    $sql = "SELECT COUNT(*) AS `amountToAuthorize` FROM `Transfer` WHERE `authorizedBy` IS NULL";
+    $result = mysqli_query($stmt);
+    $amountToAuthorize = mysqli_fetch_all($result)[0]["amountToAuthorize"];
+
+
+    $sql = "SELECT COUNT(*) FROM `Transfer` WHERE `authorizedBy` IS NOT NULL AND `madeBy` IS NULL";
+    $result = mysqli_query($stmt);
+    $amountToMade = mysqli_fetch_all($result, MYSQLI_ASSOC)[0]["amountToMade"];
 
     $view = file_get_contents("./page.html");
+
+    $view = str_replace($view, "[TRANSFERS_AMOUNT_TO_AUTHORIZE]", $amountToAuthorize);
+    $view = str_replace($view, "[TRANSFERS_AMOUNT_TO_MADE]", $amountToMade);
+
     echo $view;
 ?>
