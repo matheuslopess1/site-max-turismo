63ddd8274a72be234be8b5da9b513c7f9c2d4a18
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-02-22 02:11:21 -0300

    atualiza index de dashboard

--- panel/dashboard/index.php
+++ panel/dashboard/index.php
@@ -4,16 +4,14 @@
     include_once("./../components/database.php");
 
     $sql = "SELECT COUNT(*) AS `amountToAuthorize` FROM `Transfer` WHERE `authorizedBy` IS NULL";
-    $result = mysqli_query($sql);
+    $result = mysqli_query($link, $sql);
     $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    var_dump($rows);
     $amountToAuthorize = $rows[0]["amountToAuthorize"];
 
 
     $sql = "SELECT COUNT(*) FROM `Transfer` WHERE `authorizedBy` IS NOT NULL AND `madeBy` IS NULL";
-    $result = mysqli_query($sql);
+    $result = mysqli_query($link, $sql);
     $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    var_dump($rows);
     $amountToMade = $rows[0]["amountToMade"];
 
     $view = file_get_contents("./page.html");
