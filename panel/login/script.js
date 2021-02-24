d6cfdf60cae1e9df27c4e110dd2b951363fc18fc
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-02-23 23:58:35 -0300

    atualiza script de login

--- panel/login/script.js
+++ panel/login/script.js
@@ -1,11 +1,11 @@
 
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search)
 
-if (urlParams.has("error")) {
-    const containerElement = document.querySelector("#container");
-    const error = urlParams.get("error");
-    const div = document.createElement("div");
-    div.classList.add("text-danger", "text-center");
+if (urlParams.has('error')) {
+    const containerElement = document.querySelector('#container')
+    const error = urlParams.get('error')
+    const div = document.createElement('div')
+    div.classList.add('text-danger', 'text-center')
     div.innerText = error
-    containerElement.appendChild(div);
+    containerElement.appendChild(div)
 }
