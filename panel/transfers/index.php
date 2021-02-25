c4c645f9a87c99cdfde16f805df007a5dfe3cc50
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-02-24 21:34:15 -0300

    atualiza index de transfers

--- panel/transfers/index.php
+++ panel/transfers/index.php
@@ -3,7 +3,7 @@
     include_once("../components/authenticated.php");
     include_once("../components/database.php");
 
-    $sql = "SELECT t.id, aa.name AS outgoingAccount, ab.name AS incomingAccount, t.amount, ua.name AS createdBy, ub.name AS authorizedBy, uc.name AS madeBy FROM `Transfer` t JOIN Account aa ON t.outgoingAccount = aa.id JOIN Account ab ON t.incomingAccount = ab.id JOIN User ua ON t.createdBy = ua.id LEFT JOIN User ub ON t.authorizedBy = ub.id LEFT JOIN User uc ON t.madeBy = uc.id";
+    $sql = "SELECT t.id, aa.shortName AS outgoingAccount, ab.shortName AS incomingAccount, t.amount, ua.shortName AS createdBy, ub.shortName AS authorizedBy, uc.shortName AS madeBy FROM `Transfer` t JOIN Account aa ON t.outgoingAccount = aa.id JOIN Account ab ON t.incomingAccount = ab.id JOIN User ua ON t.createdBy = ua.id LEFT JOIN User ub ON t.authorizedBy = ub.id LEFT JOIN User uc ON t.madeBy = uc.id";
     $result = mysqli_query($link, $sql);
     $transfers = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
@@ -16,11 +16,11 @@
     $outgoingRegisters = [$registerMatheus, $registerNSA, $registerEirelli];
     $outgoingRegisters = implode($glue, array_map($addQuotes, $outgoingRegisters));
 
-    $sql = "SELECT id, `name`, IF(`type` = 'checking', 'CC', 'CP') AS `type`, bank, agency, account FROM Account WHERE register IN ($outgoingRegisters)";
+    $sql = "SELECT id, shortName, IF(`type` = 'checking', 'CC', 'CP') AS `type`, bank, agency, account FROM Account WHERE register IN ($outgoingRegisters)";
     $result = mysqli_query($link, $sql);
     $outgoingAccounts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    $sql = "SELECT id, `name`, IF(`type` = 'checking', 'CC', 'CP') AS `type`, bank, agency, account FROM Account WHERE register NOT IN ($outgoingRegisters)";
+    $sql = "SELECT id, shortName, IF(`type` = 'checking', 'CC', 'CP') AS `type`, bank, agency, account FROM Account WHERE register NOT IN ($outgoingRegisters)";
     $result = mysqli_query($link, $sql);
     $incomingAccounts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
