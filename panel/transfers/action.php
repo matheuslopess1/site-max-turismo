9ad01886cd9722743a7802009ca0807cd5fe5258
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-02-24 21:11:07 -0300

    atualiza transfers

--- panel/transfers/action.php
+++ panel/transfers/action.php
@@ -30,18 +30,41 @@
     }
 
     if (is_numeric($_POST["amount"]) === FALSE) {
-        header("Location: /panel/transfers/?error=Parâmetro deve ser composto apenas por números com casas decimais identificados por um . (ponto): amount");
+        header("Location: /panel/transfers/?error=Parâmetro amount deve ser composto apenas por números com casas decimais identificados por um . (ponto)");
         exit();
     }
 
     if ($_POST["amount"] <= 0.00) {
-        header("Location: /panel/transfers/?error=Parâmetro deve ser composto por números positivos");
+        header("Location: /panel/transfers/?error=Parâmetro amount deve ser composto por números positivos");
         exit();
     }
 
     $outgoingAccount = $_POST["outgoing_account"];
     $incomingAccount = $_POST["incoming_account"];
     $amount = $_POST["amount"];
+
+    $registerMatheus = "114.234.744-30";
+    $registerNSA = "34.803.364/0001-21";
+    $registerEirelli = "29.995.023/0001-65";
+
+    $glue = ", ";
+    $addQuotes = function($item) { return "'$item'"; };
+    $outgoingRegisters = [$registerMatheus, $registerNSA, $registerEirelli];
+    $outgoingRegisters = implode($glue, array_map($addQuotes, $outgoingRegisters));
+
+    $sql = "SELECT (SELECT COUNT(*) FROM Account WHERE register IN ($outgoingRegisters) AND id = $outgoingAccount) AS outgoingAccountCounter, (SELECT COUNT(*) FROM Account WHERE register NOT IN ($outgoingRegisters) AND Id = $incomingAccount) AS incomingAccountCounter";
+    $result = mysqli_query($link, $sql);
+    [$outgoingAccountCounter, $incomingAccountCounter] = mysqli_fetch_all($result, MYSQLI_ASSOC)[0];
+
+    if ($outgoingAccountCounter === 0) {
+        header("Location: /panel/transfers/?error=Parâmetro outgoingAccount contém um valor inválido");
+        exit();
+    }
+
+    if ($incomingAccountCounter === 0) {
+        header("Location: /panel/transfers/?error=Parâmetro incomingAccountCounter contém um valor inválido");
+        exit();
+    }
 
     $userId = $_SESSION["user"]["id"];
 
